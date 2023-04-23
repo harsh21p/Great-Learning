15d5aac4ca51cb30211b57aa86de8b55303e8394
Harshad Ramesh PACHORE <harshadpa@MTLP-2131.local>
2023-04-24 00:01:32 +0530

    Submit

---
+++ G4_B2_JS_GradedProject2/Backend/login.js
@@ -0,0 +1,36 @@
+const LoggedIn = sessionStorage.getItem("userLoggedIn");
+
+function checkLogin(LoggedIn, hrefUrl) {
+  if (LoggedIn) {
+    alert("user already authenticated!");
+    window.location.replace(hrefUrl);
+  }
+}
+checkLogin(LoggedIn, "./resume.html");
+
+const userDetails = {
+  username: "admin",
+  password: "admin@123",
+};
+
+localStorage.setItem("userCred", JSON.stringify(userDetails));
+const getCred = JSON.parse(localStorage.getItem("userCred"));
+
+const passVer = () => {
+  const user = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  const LoginError = document.getElementById("loginError");
+  const loginForm = document.getElementById("loginForm");
+
+  if (user == "" && password == "") {
+   alert("Please enter username and password!");
+  } else if (getCred["username"] == user && getCred["password"] == password) {
+    sessionStorage.setItem("userLoggedIn", true);
+    window.location.replace("./resume.html");
+  } else {
+    loginForm.reset();
+    alert("Incorrect password or username!");
+  }
+};
+
+document.getElementById("submitbutton").addEventListener("click", passVer);
